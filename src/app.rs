feb1567cc365e7b180059d88d18879383b1b4f4a
Piotr Szokalski <piotr.szokalski0022@gmail.com>
2023-11-20 10:24:03 +0100

    test2

--- src/app.rs
+++ src/app.rs
@@ -1,22 +1,40 @@
-use crate::sva_shell::SVAShell;
+use egui::Button;
+use egui_code_editor::{CodeEditor, ColorTheme, Syntax};
+
+use simple_virtual_assembler::instruction::Instruction;
+use simple_virtual_assembler::virtual_machine::VirtualMachine;
+
+use simple_virtual_assembler::assembler::Assembler;
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct SvaUI {
-    // Example stuff:
-    label: String,
+    tex_t: String,
 
     #[serde(skip)] // This how you opt-out of serialization of a field
     value: f32,
+
+    code: String,
+
+    vm: VirtualMachine,
+
+    acc_label: String,
+    err_buffer: String,
 }
 
 impl Default for SvaUI {
     fn default() -> Self {
         Self {
             // Example stuff:
-            label: "Hello World!".to_owned(),
+            tex_t: "Hello World!".to_string(),
             value: 2.7,
+
+            code: String::new(),
+            vm: VirtualMachine::new(vec![]),
+
+            acc_label: String::new(),
+            err_buffer: String::new(),
         }
     }
 }
@@ -70,11 +88,6 @@ impl eframe::App for SvaUI {
             // The central panel the region left after adding TopPanel's and SidePanel's
             ui.heading("eframe template");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
             ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
             if ui.button("Increment").clicked() {
                 self.value += 1.0;
@@ -83,18 +96,52 @@ impl eframe::App for SvaUI {
             ui.separator();
 
             ////
-            let mut vm = SVAShell::new(0);
-            vm.show(&ctx);
+            //let mut vm = SVAShell::new(0);
 
-            /// 
+            egui::Window::new("vm").show(ctx, |ui| {
+                ui.vertical(|ui| {});
 
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
+                ui.horizontal(|ui| {
+                    if ui.button("run").clicked() {
+                        //TEMP
 
-            ui.label("test");
+                        let res = Assembler::new().parse(&self.code);
 
+                        match res {
+                            Ok(program) => {
+                                let mut vm2 = VirtualMachine::new(program);
+                                vm2.run();
+    
+                                self.acc_label = vm2.get_acc().to_string();
+                                self.err_buffer.clear();
+                            }
+                            Err(err) => {
+                                self.err_buffer = err.to_string();
+                            }
+                        }
+    
+                    }
+                    
+                   
+                    //assemble_and_run(&mut self.vm, &self.code, &mut self.tex_t);
+                });
+                CodeEditor::default()
+                    .id_source("code editor")
+                    .with_rows(12)
+                    .with_fontsize(14.0)
+                    .with_theme(ColorTheme::GRUVBOX)
+                    .with_syntax(Syntax::rust())
+                    .with_numlines(true)
+                    .show(ui, &mut self.code);
+
+                ui.label("acc");
+                ui.button(self.acc_label.clone());
+                ui.button("pc");
+
+                ui.label(&self.err_buffer);
+
+               // ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {});
+            });
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 powered_by_egui_and_eframe(ui);
@@ -102,6 +149,30 @@ impl eframe::App for SvaUI {
             });
         });
     }
+}
+
+fn assemble_and_run(vm: &mut VirtualMachine, code_text: &String, mut err_output: &mut String) {
+    let mut assembler = Assembler::new();
+
+    let result = assembler.parse(&code_text);
+
+    // match result {
+    //     Ok(program) => run(vm, program),
+    //     Err(err) => err_output = &mut err.to_string(),
+    // }
+}
+
+fn run(vm: &mut VirtualMachine, instructions: Vec<Instruction>) {
+    // vm.load_program(instructions);
+
+    let mut x = VirtualMachine::new(vec![]);
+    // x.load_program(instructions);
+
+    // vm.load_program(vec![]);
+
+    // *vm = VirtualMachine::new(instructions);
+
+    // vm.run();
 }
 
 fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
--- src/sva_shell.rs
+++ src/sva_shell.rs
@@ -5,6 +5,8 @@ use egui::{Align, Slider, TextEdit, Ui, Widget};
 
 use simple_virtual_assembler::virtual_machine::VirtualMachine;
 
+use egui_code_editor::{CodeEditor, ColorTheme, Syntax};
+
 use simple_virtual_assembler::assembler::Assembler;
 #[derive(serde::Deserialize, serde::Serialize)]
 pub struct SVAShell {
@@ -14,26 +16,48 @@ pub struct SVAShell {
     code: String,
 }
 
+impl Default for SVAShell {
+    fn default() -> Self {
+        Self {
+            id: 0,
+            sva: VirtualMachine::new(vec![]),
+            //assembler: Assembler {},
+            code: "".to_string(),
+        }
+    }
+}
+
 impl SVAShell {
     pub fn new(id: i32) -> SVAShell {
         SVAShell {
             id,
             sva: VirtualMachine::new(vec![]),
             //assembler: Assembler {},
-            code: "".to_string(),
+            code: "CODE".to_string(),
         }
     }
 
-    pub fn show(&mut self, ctx: &egui::Context) {
-        egui::Window::new(&self.id.to_string()).show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                ui.label("Your name: ");
-                ui.text_edit_multiline(&mut self.code);
-                ui.text_edit_singleline(&mut "&mut self.name");
-            });
+    pub fn show(&mut self, ctx: &Context, ui: &mut Ui) {
+        Window::new(format!("Window {}", self.id)).show(ctx, |ui| self.ui_content(ui));
 
-            if ui.button("Click each year").clicked() {}
-            ui.label(format!("Hello '', age "));
+        // Handle button click outside the window
+        if ui.button("X").clicked() {
+            // Handle button click action
+        }
+    }
+    
+
+    fn ui_content(&mut self, ui: &mut Ui) {
+        ui.horizontal(|ui| {
+            ui.label("Editable Text:");
+            CodeEditor::default()
+                .id_source("code editor")
+                .with_rows(12)
+                .with_fontsize(10.0)
+                .with_theme(ColorTheme::GRUVBOX)
+                .with_syntax(Syntax::rust())
+                .with_numlines(true)
+                .show(ui, &mut self.code);
         });
     }
 }
@@ -42,7 +66,7 @@ impl Widget for SVAShell {
     fn ui(self, ui: &mut Ui) -> egui::Response {
         // Use Egui API to create your custom widget UI
 
-        let response = Label::new("avxc").ui(ui);
+        let response = Label::new("ascx").ui(ui);
         // Perform any additional actions or logic for your widge
         response
     }
