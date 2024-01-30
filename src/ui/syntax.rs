43025e06ce3861b8fccfa088d2ef2d829b791a25
Piotr Szokalski <piotr.szokalski0022@gmail.com>
2024-01-30 23:04:31 +0100

    tweaing syntax highhlgith

--- src/ui/syntax.rs
+++ src/ui/syntax.rs
@@ -13,10 +13,12 @@ pub fn sva_syntax() -> Syntax {
         keywords: BTreeSet::from([
             "nop", "NOP", "hlt", "HLT", "mov", "MOV", "add", "ADD", "sub", "SUB", "mul", "MUL",
             "div", "DIV", "mod", "MOD", "inc", "INC", "dec", "DEC", "and", "AND", "or", "OR",
-            "xor", "XOR", "not", "NOT", "shl", "SHL", "shr", "SHR", "cmp", "CPM", "jmp", "JMP",
-            "je", "JE", "jne", "JNE", "jl", "JL", "jg", "JG", "psh", "PSH", "pop", "POP",
+            "xor", "XOR", "not", "NOT", "shl", "SHL", "shr", "SHR", "cmp", "CPM", "psh", "PSH",
+            "pop", "POP",
         ]),
-        types: BTreeSet::from([]),
+        types: BTreeSet::from([
+            "jmp", "JMP", "je", "JE", "jne", "JNE", "jl", "JL", "jg", "JG",
+        ]),
         special: BTreeSet::from([]),
     }
 }
