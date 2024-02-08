262e9b5f0c8afa939e7e2224a845b1f801b70428
Piotr Szokalski <piotr.szokalski0022@gmail.com>
2024-02-08 18:37:14 +0100

    removing unecsearry libarlies

--- src/main.rs
+++ src/main.rs
@@ -40,10 +40,6 @@ fn main() -> eframe::Result<()> {
         // You can customize this to save the error message to a different file or perform any other action
     }));
 
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-
-    let _logger = CustomLogger::new();
-
     let native_options = eframe::NativeOptions {
         //initial_window_size: Some([400.0, 300.0].into()),
         //min_window_size: Some([300.0, 220.0].into()),
